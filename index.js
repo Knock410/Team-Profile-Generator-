46eb3067926e4734770e4f3c9f92f59689a15856
Knock <enoch10evelyn@gmail.com>
2022-10-09 22:36:39 -0700

    Created Manager Prompt for questions

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const team = []
 //  const generateHtml = require();
 
 
-// const managerQuestions =
- inquirer.prompt([
+ const managerQuestions = [
     {
       type: "input",
       name: "managerName",
@@ -27,72 +26,70 @@ const team = []
       name: "officeNumber",
       message: "What is the manager's office number?",
     },
-  // ];
+   ];
   //adds manager data to the team array 
-  // function promptManager() {
-  //   inquirer.prompt(managerQuestions).then((answers) => {
-  //     console.log(answers);
-  //     const manager = new Manager(answers.managerName, answers.employeeId, answers.email, answers.officeNumber)
-  //     team.push(manager)
-  //     promptMenu();
-  //   });
-  // }
-  // promptManager();
-//menu questions
-  // const additonalTeamMemebersQuestions = [
-    {
-      type: "list",
-      name: "additionalEmployees",
-      message: "Would you like to add the following team members?",
-      choices: ["Engineer", "Intern", "None"],
-    },
+  function managerPrompt(){
+    inquirer.prompt(managerQuestions).then((answers) => {
+      console.log(answers);
+    });
+  }
 
-    // const engineerQuestions = [
-      {
-        type: "input",
-        name: "engineerName",
-        message: "What is the engineer's name?",
-      },
-      {
-        type: "input",
-        name: "engineerId",
-        message: "What is their employee ID?",
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "What is their email address?",
-      },
-      {
-        type: "input",
-        name: "gitHub",
-        message: "What is their GitHub username?",
-      },
-    // ];
+  managerPrompt();
+// //menu questions
+//    const additonalTeamMemebersQuestions = [
+//     {
+//       type: "list",
+//       name: "additionalEmployees",
+//       message: "Would you like to add the following team members?",
+//       choices: ["Engineer", "Intern", "None"],
+//     },
 
-    // const internQuestions = [
-      {
-        type: "input",
-        name: "internName",
-        message: "What is the intern's name?",
-      },
-      {
-        type: "input",
-        name: "internId",
-        message: "What is their employee ID?",
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "What is their email address?",
-      },
-      {
-        type: "input",
-        name: "school",
-        message: "What school did they go to?",
-      },
-    // ];
-  ]);
+//     // const engineerQuestions = [
+//       {
+//         type: "input",
+//         name: "engineerName",
+//         message: "What is the engineer's name?",
+//       },
+//       {
+//         type: "input",
+//         name: "engineerId",
+//         message: "What is their employee ID?",
+//       },
+//       {
+//         type: "input",
+//         name: "email",
+//         message: "What is their email address?",
+//       },
+//       {
+//         type: "input",
+//         name: "gitHub",
+//         message: "What is their GitHub username?",
+//       },
+//   ];
+
+//      const internQuestions = [
+//       {
+//         type: "input",
+//         name: "internName",
+//         message: "What is the intern's name?",
+//       },
+//       {
+//         type: "input",
+//         name: "internId",
+//         message: "What is their employee ID?",
+//       },
+//       {
+//         type: "input",
+//         name: "email",
+//         message: "What is their email address?",
+//       },
+//       {
+//         type: "input",
+//         name: "school",
+//         message: "What school did they go to?",
+//       },
+//      ];
+
 
 
 
@@ -104,5 +101,20 @@ const team = []
 //     console.log('Your team is ready!');
 //  });
 // }
+
+
+// function promptMenu() {
+//   inquirer.prompt(additonalTeamMemebersQuestions).then((answers) => {
+//     console.log(answers);
+//     if (answers.otherEmployees === "Engineer") {
+//       promptEngineer();
+//        .then(answers => {
+//           console.log(answers);
+//     }
+//     if (answers.otherEmployees === "Intern") {
+//       promptIntern();
+//        .then(answers => {
+//           console.log(answers);
+//     }
   
-  
+//  promptMenu();
