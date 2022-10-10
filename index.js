b1948f90a46a8b4b8e382ca2dc97250dcc6d2be4
Knock <enoch10evelyn@gmail.com>
2022-10-09 21:41:15 -0700

    wrote fs code, additional team member code and tested if the question works in sequence

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const inquirer = require('inquirer');
+const team = []
+ const fs = require("fs");
+//  const generateHtml = require();
 
 
-const managerQuestions = [
+// const managerQuestions =
+ inquirer.prompt([
     {
       type: "input",
       name: "managerName",
@@ -23,14 +27,36 @@ const managerQuestions = [
       name: "officeNumber",
       message: "What is the manager's office number?",
     },
-  ];
+  // ];
   
-  function promptManager() {
-    inquirer.prompt(managerQuestions).then((answers) => {
-      console.log(answers);
-      const manager = new Manager(answers.managerName, answers.employeeId, answers.email, answers.officeNumber)
-      team.push(manager)
-      promptMenu();
-    });
-  }
-  promptManager();
+  // function promptManager() {
+  //   inquirer.prompt(managerQuestions).then((answers) => {
+  //     console.log(answers);
+  //     const manager = new Manager(answers.managerName, answers.employeeId, answers.email, answers.officeNumber)
+  //     team.push(manager)
+  //     promptMenu();
+  //   });
+  // }
+  // promptManager();
+
+  // const additonalEmployeesQuestions = [
+    {
+      type: "list",
+      name: "additionalEmployees",
+      message: "Would you like to add the following additonal employees?",
+      choices: ["Engineer", "Intern", "None"],
+    },
+  ]);
+
+
+
+
+//  function makeHtml(){ 
+//   fs.writeFile('index.html', generateHtml(team),err => {
+//     if (err) throw err;
+  
+//     console.log('Your team is ready!');
+//  });
+// }
+  
+  
