c21095f46ac4853db691ef56b483077e9482a1c7
Knock <enoch10evelyn@gmail.com>
2022-10-09 22:06:18 -0700

    Questions created

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const team = []
       message: "What is the manager's office number?",
     },
   // ];
-  
+  //adds manager data to the team array 
   // function promptManager() {
   //   inquirer.prompt(managerQuestions).then((answers) => {
   //     console.log(answers);
@@ -38,14 +38,60 @@ const team = []
   //   });
   // }
   // promptManager();
-
-  // const additonalEmployeesQuestions = [
+//menu questions
+  // const additonalTeamMemebersQuestions = [
     {
       type: "list",
       name: "additionalEmployees",
-      message: "Would you like to add the following additonal employees?",
+      message: "Would you like to add the following team members?",
       choices: ["Engineer", "Intern", "None"],
     },
+
+    // const engineerQuestions = [
+      {
+        type: "input",
+        name: "engineerName",
+        message: "What is the engineer's name?",
+      },
+      {
+        type: "input",
+        name: "engineerId",
+        message: "What is their employee ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is their email address?",
+      },
+      {
+        type: "input",
+        name: "gitHub",
+        message: "What is their GitHub username?",
+      },
+    // ];
+
+    // const internQuestions = [
+      {
+        type: "input",
+        name: "internName",
+        message: "What is the intern's name?",
+      },
+      {
+        type: "input",
+        name: "internId",
+        message: "What is their employee ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is their email address?",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "What school did they go to?",
+      },
+    // ];
   ]);
 
 
